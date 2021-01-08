bedb2dfd198a87ef62f820ead444523b037e8989
Jansen Rodriguez <JansenR125@gmail.com>
2021-01-08 14:51:03 -0500

    Refactored to use bean scope annotations

--- com/luv2code/springdemo/TennisCoach.java
+++ com/luv2code/springdemo/TennisCoach.java
@@ -1,7 +1,11 @@
 package com.luv2code.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -24,5 +28,16 @@ public class TennisCoach implements Coach {
 	public String getDailyFortune() {
 		return fortuneService.getFortune();
 	}
-
+	
+	// Init Method
+	@PostConstruct
+	public void initialize() {
+		System.out.println("Init method called ");
+	}
+	
+	// Destroy Method
+	@PreDestroy
+	public void destroy() {
+		System.out.println("Destroy method called ");
+	}
 }
